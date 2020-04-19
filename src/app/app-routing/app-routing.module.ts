2ca95b9253e965034282ea652070b582e10054f1
klcun94 <klcun94@icloud.com>
2020-04-19 17:17:18 -0500

    routing added

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BoardComponent } from '../board/board/board.component';
-import { TodotableComponent } from '../todotable/todotable.component'
+import { TodotableComponent } from '../todotable/todotable.component';
 
 const routes: Routes = [
   {
